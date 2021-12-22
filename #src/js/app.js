e6c0278bd88eec0069665fbfaefc78ea9404f18f
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2021-12-22 13:38:51 +0300

    rebuild config

--- #src/js/app.js
+++
@@ -1,7 +0,0 @@
-@@include('files/regular.js', {});
-@@include('files/sliders.js', {}); //лучше подключать его повыше
-@@include('files/functions.js', {});
-@@include('files/script.js', {});
-@@include('files/forms.js', {});
-@@include('files/scroll.js', {});
-@@include('files/map.js', {});
--- #src/js/vendors.js
+++
@@ -1,3 +0,0 @@
-@@include('libs/swiper.min.js', {});
-
-
